ff6933e71309e393e7f31ad43a4eedb35b7780f7
RafoDev <rafael.david@ucsp.edu.pe>
2024-09-30 23:00:41 -0500

    feat(web-scraping): Avance 03.

--- web-scraping/03-ml-scrapper.py
+++ web-scraping/03-ml-scrapper.py
@@ -27,11 +27,28 @@ def get_products(query):
   product_list = html.find_all("li", {"class":"ui-search-layout__item shops__layout-item"})
 
   for product in product_list:
-    # titulo, precio, descuento, calificacion, reviews, variaciones, info delivery, best seller
     element_title = product.h2
-    product_title = element_title.get_text()
     
-    print(product_title)
+    product_title = element_title.get_text()
+    product_price = float(product.find(class_="andes-money-amount andes-money-amount--cents-superscript").get_text()[2:].replace(',', '.'))
+    shipping_info = product.find(class_="poly-component__shipping")
+    product_free_shipping = True if shipping_info and shipping_info.get_text().find("gratis") else False
+
+    discount_info = product.find(class_="andes-money-amount__discount")
+    product_discount = discount_info.string if discount_info else "0"
+    product_discount = int(product_discount.replace("% OFF",""))
+
+    rating_info = product.find(class_="poly-reviews__rating")
+    product_rating = float(rating_info.string) if rating_info else 0
+      
+    reviews_info = product.find(class_="poly-reviews__total")
+    product_reviews = int(reviews_info.string[1: -1]) if reviews_info else 0
+
+    variations_info = product.find(class_="poly-component__variations-text")
+    product_variations = int(variations_info.string.split()[2]) if variations_info else 0
+
+    print((product_title, product_price, product_discount, product_free_shipping, product_rating, product_reviews, product_variations))
+    # best seller
 
 get_products("teclado")
 # soup = BeautifulSoup(html_doc)
