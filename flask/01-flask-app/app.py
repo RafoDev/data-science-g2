0d61e8bff4aeafeff3694a242cb2772b550e48b5
RafoDev <rafael.david@ucsp.edu.pe>
2025-02-12 21:21:12 -0500

    feat: url parameters

--- flask/01-flask-app/app.py
+++ flask/01-flask-app/app.py
@@ -18,4 +18,26 @@ def suma():
   resultado = int(n1) + int(n2)
   return f"<h1>El resultado de la suma es {resultado}!</h2>"
 
+@app.route('/resta/<int:n1>/<int:n2>')
+def resta(n1=0, n2=0):
+  resultado = n1 - n2
+  return f"<h1>El resultado de la resta es {resultado}!</h2>"
+
+@app.route('/<ope>/<int:n1>/<int:n2>')
+def operaciones(ope="nn", n1=0, n2=0):
+  if ope == "suma":
+    resultado = n1 + n2
+  elif ope == "resta":
+    resultado = n1 - n2
+  elif ope == "multiplicacion":
+    resultado = n1 * n2
+  elif ope == "division":
+    resultado = n1 / n2
+  else:
+    resultado = "nn"
+
+  if resultado == "nn":
+    return f"<h1>No se encontro la operacion: {ope}</h2>"   
+  return f"<h1>El resultado de la {ope} es {resultado}!</h2>"
+
 app.run(debug=True)
