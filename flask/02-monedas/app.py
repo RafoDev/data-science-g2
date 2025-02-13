f30efbe50e1703a520354181733cb173d3f7cd78
RafoDev <rafael.david@ucsp.edu.pe>
2025-02-12 21:54:59 -0500

    feat: app para convertir monedas

--- flask/02-monedas/app.py
+++ flask/02-monedas/app.py
@@ -2,9 +2,19 @@ from flask import Flask,request, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
   author = request.args.get("author", "unknown")
-  return render_template("index.html", author=author)
+
+  monto_destino = 0
+
+  if request.method == "POST":
+    monto_origen = request.form["monto_origen"]
+    monto_destino = round(int(monto_origen) / 3.7, 2)
+
+  return render_template(
+    "index.html", 
+    author=author, 
+    monto_destino=monto_destino)
 
 app.run(debug=True)
