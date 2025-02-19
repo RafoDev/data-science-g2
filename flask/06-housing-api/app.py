53a402e0508a9f5cfdb1b4e3fc2116265438babd
RafoDev <rafael.david@ucsp.edu.pe>
2025-02-19 17:23:34 -0500

    feat: agregar get_data y set_data a housing-api

--- flask/06-housing-api/app.py
+++ flask/06-housing-api/app.py
@@ -1,5 +1,6 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
 app.app_context().push()
@@ -11,13 +12,19 @@ db = SQLAlchemy(app)
 
 class Housing(db.Model):
   id = db.Column(db.Integer, primary_key=True)
-  rooms = db.Column(db.Integer, primary_key=True)
-  price = db.Column(db.Double, primary_key=True)
+  rooms = db.Column(db.Integer, nullable=False)
+  price = db.Column(db.Double, nullable=True)
 
   def __init__(self, rooms):
     self.rooms = rooms
 
 db.create_all()
+
+ma = Marshmallow(app)
+class HousingSchema(ma.Schema):
+  class Meta:
+    fields = ("id", "rooms", "price")
+
 
 @app.route("/")
 def index():
@@ -25,3 +32,45 @@ def index():
     "message": "api v1.0"
   }
   return jsonify(context)
+
+@app.route("/housing", methods=["POST"])
+def set_data():
+  rooms = request.json["rooms"]
+  new_housing = Housing(rooms)
+  db.session.add(new_housing)
+  db.session.commit()
+
+  data_schema = HousingSchema()
+
+  context = {
+    "status": True,
+    "message": "New housing created!",
+    "content": data_schema.dump(new_housing)
+  }
+  return jsonify(context)
+
+@app.route('/housing', methods=["GET"])
+def get_data():
+  data = Housing.query.all()
+  data_schema = HousingSchema(many=True)
+
+  context = {
+    "status": True,
+    "message": "All the housing data",
+    "content": data_schema.dump(data)
+  }
+
+  return context
+
+@app.route('/housing/<id>')
+def get_data_by_id(id):
+  data = Housing.query.get(id)
+  data_schema = HousingSchema()
+
+  context = {
+    "status": True,
+    "message": "Requested register",
+    "content": data_schema.dump(data)
+  }
+
+  return context
