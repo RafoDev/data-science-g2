ec9c15075d95a2b028d418b53e01f6a7934a55f8
RafoDev <rafael.david@ucsp.edu.pe>
2025-02-21 20:48:06 -0500

    feat: configuracion de la db y marshmallow

--- flask/08-api-rest/app/__init__.py
+++ flask/08-api-rest/app/__init__.py
@@ -1,7 +1,17 @@
 from flask import Flask
 from .api import api
+from .config import Config
+from .extensions import db
 
 def create_app():
   app = Flask(__name__)
+  
+  app.config.from_object(Config)
+  db.init_app(app)
+
   app.register_blueprint(api)
+
+  with app.app_context():
+    db.create_all()
+    
   return app
