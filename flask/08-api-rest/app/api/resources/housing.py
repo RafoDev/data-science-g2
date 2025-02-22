1c406ba36aada414d8e237eb6a660402b0e56c3d
RafoDev <rafael.david@ucsp.edu.pe>
2025-02-21 22:06:24 -0500

    feat: resource para housing por id

--- flask/08-api-rest/app/api/resources/housing.py
+++ flask/08-api-rest/app/api/resources/housing.py
@@ -36,5 +36,47 @@ class HousingResource(Resource):
     }
     return context
   
+class HousingResourceDetail(Resource):
+  def get(self, id):
+    data = Housing.get_by_id(id)
+    data_schema = HousingSchema()
+
+    context = {
+      "status": True,
+      "message": "Registro encontrado",
+      "content": data_schema.dump(data)
+    }
+    return context
+
+  def put(self, id):
+    data = request.get_json()
+    rooms = int(data["rooms"])
+
+    housing = Housing.get_by_id(id)
+    housing.rooms = rooms
+    housing.save()
+
+    data_schema = HousingSchema()
+
+    context = {
+      "status": True,
+      "message": "Registro actualizado",
+      "content": data_schema.dump(housing)
+    }
+    return context
+  
+  def delete(self, id):
+    housing = Housing.get_by_id(id)
+    housing.delete()
+
+    data_schema = HousingSchema()
+
+    context = {
+      "status": True,
+      "message": "Registro eliminado",
+      "content": data_schema.dump(housing)
+    }
+    return context
 
 api_housing.add_resource(HousingResource,"/housing")
+api_housing.add_resource(HousingResourceDetail,"/housing/<id>")
