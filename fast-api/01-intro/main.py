bdb6888f37f0f602b7380afeca51761992b55f7b
RafoDev <rafael.david@ucsp.edu.pe>
2025-02-26 19:40:27 -0500

    feat: ruta de saludo y suma

--- fast-api/01-intro/main.py
+++ fast-api/01-intro/main.py
@@ -5,3 +5,12 @@ app = FastAPI()
 @app.get("/")
 async def root():
   return {"message": "Mi primer api con FastApi"}
+
+@app.get("/saludo/{nombre}")
+async def saludo(nombre: str):
+  return {"message": f"Hola {nombre}"}
+
+@app.get("/suma/{n1}/{n2}")
+async def suma(n1: int, n2: int):
+  resultado = n1 + n2
+  return {"message": f"La suma de {n1} y {n2} es {resultado}"}
