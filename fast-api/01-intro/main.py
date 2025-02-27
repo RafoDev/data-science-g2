46e649fcec1c6b785165f4c10e687583b1c6e18b
RafoDev <rafael.david@ucsp.edu.pe>
2025-02-26 19:57:55 -0500

    feat: integracion con sqlmodel y modelo de Housing

--- fast-api/01-intro/main.py
+++ fast-api/01-intro/main.py
@@ -1,7 +1,25 @@
 from fastapi import FastAPI
+from sqlmodel import create_engine, SQLModel, Field
+from pydantic import BaseModel
+
+DATABASE_URI = "mysql+mysqlconnector://root:1234@localhost/ml_db"
+engine = create_engine(DATABASE_URI, echo=True)
+
+class Housing(SQLModel, table=True):
+  id: int = Field(default=None, primary_key=True)
+  rooms: int
+  price: float
+
+class HousingCreate(BaseModel):
+  rooms: int
+  price: float
+
+class HousingCreate(HousingCreate):
+  id: int
+
+SQLModel.metadata.create_all(bind=engine)
 
 app = FastAPI()
-
 @app.get("/")
 async def root():
   return {"message": "Mi primer api con FastApi"}
