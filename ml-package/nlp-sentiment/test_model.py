cb6e2c39622d922ce2671b8c1d1ff044ca833750
RafoDev <rafael.david@ucsp.edu.pe>
2025-03-03 20:47:41 -0500

    feat: simplificacion de modelo

--- ml-package/nlp-sentiment/test_model.py
+++ ml-package/nlp-sentiment/test_model.py
@@ -6,23 +6,22 @@ path = "./model"
 tokenizer = AutoTokenizer.from_pretrained(path)
 model = AutoModelForSequenceClassification.from_pretrained(path)
 
-def predict_sentiment(texts, model, tokenizer):
+def predict_sentiment(text, model, tokenizer):
   device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
   model = model.to(device)
 
-  inputs = tokenizer(texts, return_tensors="pt", truncation=True, padding=True, max_length=512)
+  input = tokenizer(text, return_tensors="pt", truncation=True, padding=True, max_length=512)
   
-  input_ids = inputs["input_ids"].to(device)
-  attention_mask = inputs["attention_mask"].to(device)
+  input_ids = input["input_ids"].to(device)
+  attention_mask = input["attention_mask"].to(device)
 
   with torch.no_grad():
     outputs = model(input_ids=input_ids, attention_mask=attention_mask)
   
   probabilities = torch.nn.functional.softmax(outputs.logits, dim=-1)
   sentiment_labels = {0: "Muy negativo", 1:"Negativo", 2:"Neutral", 3: "Positivo", 4:"Muy positivo"}
-  return [sentiment_labels[p] for p in torch.argmax(probabilities, dim=-1).tolist()]
+  return sentiment_labels[torch.argmax(probabilities, dim=-1).item()]
 
-texts = [ "I absolutely love the new design of this app!"]
-
-for text, sentiment in zip(texts, predict_sentiment(texts, model, tokenizer)):
-  print(f"{text} -> {sentiment}")
+text = "I absolutely love the new design of this app!"
+sentiment = predict_sentiment(text, model, tokenizer)
+print(f"{text} -> {sentiment}")
