c1d24cd91dbc843e9916359e0c5a8d072a176999
RafoDev <rafael.david@ucsp.edu.pe>
2025-03-07 22:37:22 -0500

    feat: docker compose para 02-fastapi

--- fast-api/02-api-docker/app.py
+++ fast-api/02-api-docker/app.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
+from housing_predictor.housing_predictor import HousingModel
+from pydantic import BaseModel
 
 app = FastAPI()
+housing_model = HousingModel()
+
+class HousingInput(BaseModel):
+  rooms: int
 
 @app.get("/")
 async def index():
-  return {"message": "🐋 Dockerized FastAPI v1.0"}
+  return {"message": "🐋 Dockerized FastAPI v1.1"}
+  
+@app.post("/housing")
+async def housing_predictor(input: HousingInput):
+  rooms = input.rooms
+  price = housing_model.predict(rooms)
+  return {"rooms": rooms, "price": price}
